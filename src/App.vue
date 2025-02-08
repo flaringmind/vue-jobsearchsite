c5f2a76f95bc0278795c9ad2781e121b6873ace4
flaringmind <andlukyanow@gmail.com>
2025-02-08 19:07:21 +0300

    Ready to deploy

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HomeCards from "@/components/HomeCards.vue";
+import Hero from "@/components/Hero.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+  <Hero title="Props Title" />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true"/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
